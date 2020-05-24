60c08019fb3bc19619b97faae795c77aaa986f3d
Tomo Tomov <tomotomov92@gmail.com>
2020-05-24 23:34:43 +0300

    Update DB script and remove unused models

--- Project.sql
+++ Project.sql
@@ -1,45 +1,87 @@
-CREATE DATABASE `Project` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE `CarService` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-CREATE TABLE `CarBrand` (
+
+
+CREATE TABLE `carservice`.`CarBrand` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `BrandName` varchar(45) NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `carservice`.`carbrand` (`BrandName`)
+VALUES
+('Alfa Romeo'),
+('Aston Martin'),
+('Audi'),
+('Bentley'),
+('Benz'),
+('BMW'),
+('Bugatti'),
+('Cadillac'),
+('Chevrolet'),
+('Chrysler'),
+('Citroen'),
+('Corvette'),
+('DAF'),
+('Dacia'),
+('Daewoo'),
+('Daihatsu'),
+('Datsun'),
+('De Lorean'),
+('Dino'),
+('Dodge'),
+('Farboud'),
+('Ferrari'),
+('Fiat'),
+('Ford'),
+('Honda'),
+('Hummer'),
+('Hyundai'),
+('Jaguar'),
+('Jeep'),
+('KIA'),
+('Koenigsegg'),
+('Lada'),
+('Lamborghini'),
+('Lancia'),
+('Land Rover'),
+('Lexus'),
+('Ligier'),
+('Lincoln'),
+('Lotus'),
+('Martini'),
+('Maserati'),
+('Maybach'),
+('Mazda'),
+('McLaren'),
+('Mercedes'),
+('Mercedes-Benz'),
+('Mini'),
+('Mitsubishi'),
+('Nissan'),
+('Noble'),
+('Opel'),
+('Peugeot'),
+('Pontiac'),
+('Porsche'),
+('Renault'),
+('Rolls-Royce'),
+('Rover'),
+('Saab'),
+('Seat'),
+('Skoda'),
+('Smart'),
+('Spyker'),
+('Subaru'),
+('Suzuki'),
+('Toyota'),
+('Vauxhall'),
+('Volkswagen'),
+('Volvo');
 
 
-CREATE TABLE `CarParts` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `PartDescr` varchar(100) NOT NULL,
-  `CarBrandId` int(11) NOT NULL,
-  `Price` decimal NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `fk_CarParts_CarBrand_idx` (`CarBrandId`),
-  CONSTRAINT `fk_CarParts_CarBrand` FOREIGN KEY (`CarBrandId`) REFERENCES `CarBrand` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-
-CREATE TABLE `CarServices` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `ServiceName` varchar(100) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
-CREATE TABLE `CarsSupport` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Support` varchar(1000) NOT NULL,
-  `CarBrandId` int(11) NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `fk_CarsSupport_CarBrand_idx` (`CarBrandId`),
-  CONSTRAINT `fk_CarsSupport_Car` FOREIGN KEY (`CarBrandId`) REFERENCES `CarBrand` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
-CREATE TABLE `Client` (
+CREATE TABLE `carservice`.`Client` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `FirstName` varchar(100) NOT NULL,
   `LastName` varchar(100) NOT NULL,
@@ -48,7 +90,7 @@ CREATE TABLE `Client` (
 
 
 
-CREATE TABLE `ClientCar` (
+CREATE TABLE `carservice`.`ClientCar` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `ClientId` int(11) NOT NULL,
   `CarBrandId` int(11) NOT NULL,
@@ -62,7 +104,7 @@ CREATE TABLE `ClientCar` (
 
 
 
-CREATE TABLE `Employee` (
+CREATE TABLE `carservice`.`Employee` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `FirstName` varchar(100) NOT NULL,
   `LastName` varchar(100) NOT NULL,
@@ -72,18 +114,7 @@ CREATE TABLE `Employee` (
 
 
 
-CREATE TABLE `EmployeeSalary` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `EmployeeId` int(11) NOT NULL,
-  `Salary` decimal NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `fk_EmployeeSalary_Employee_idx` (`EmployeeId`),
-  CONSTRAINT `fk_EmployeeSalary_Employee` FOREIGN KEY (`EmployeeId`) REFERENCES `Employee` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
-CREATE TABLE `InspectionHours` (
+CREATE TABLE `carservice`.`InspectionHours` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `ClientId` int(11) NOT NULL,
   `CarId` int(11) NOT NULL,
@@ -97,16 +128,7 @@ CREATE TABLE `InspectionHours` (
 
 
 
-CREATE TABLE `PriceList` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Service` varchar(100) NOT NULL,
-  `Price` decimal NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
-CREATE TABLE `Schedule` (
+CREATE TABLE `carservice`.`Schedule` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `ForDate` date NOT NULL,
   `HourBegin` decimal NOT NULL,
@@ -115,13 +137,4 @@ CREATE TABLE `Schedule` (
   PRIMARY KEY (`Id`),
   KEY `fk_Schedule_Employee_idx` (`EmployeeId`),
   CONSTRAINT `fk_Schedule_1` FOREIGN KEY (`EmployeeId`) REFERENCES `Employee` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
-CREATE TABLE `SpecialOffer` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Service` varchar(1000) NOT NULL,
-  `Price` decimal NOT NULL,
-  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
